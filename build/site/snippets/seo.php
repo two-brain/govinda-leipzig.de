a37d1eef026dc6136c5c6b69121735d25e83277b
M. Folkers <hello@twobrain.io>
2017-10-11 22:11:59 +0200

    Integrated [Kirby-Tags-Field](https://github.com/afbora/Kirby-Tabs-Field)

--- build/site/snippets/seo.php
+++ build/site/snippets/seo.php
@@ -6,11 +6,11 @@
 <!-- Facebook -->
 <meta property="og:locale" content="<?= $site->language()->locale() ?>">
 <meta property="og:type" content="website">
-<meta property="og:title" content="<?php if($page->isHomePage()) : ?><?= seo('title', array(), true); ?><?php else : ?><?= seo('title', array(), true); ?> | <?= $site->title()->html() ?><?php endif ?>">
+<meta property="og:title" content="<?= seo('title', array(), true); ?>">
 <meta property="og:description" content="<?= seo('description', array(), true); ?>">
 <meta property="og:url" content="<?= $page->url() ?>">
 <meta property="og:site_name" content="<?= $site->title()->html() ?>">
 
 <!-- Twitter -->
 <meta name="twitter:card" content="summary">
-<meta name="twitter:title" content="<?php if($page->isHomePage()) : ?><?= seo('title', array(), true); ?><?php else : ?><?= seo('title', array(), true); ?> | <?= $site->title()->html() ?><?php endif ?>">
+<meta name="twitter:title" content="<?= seo('title', array(), true); ?>">
