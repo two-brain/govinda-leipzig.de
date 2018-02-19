c1a9aa512ac2adfbebaf83a63a99b8507d5c3f2d
M. Folkers <hello@twobrain.io>
2018-02-19 08:13:53 +0100

    Implementing basic products view

--- build/site/templates/products.php
+++ build/site/templates/products.php
@@ -1,7 +1,7 @@
 <?php snippet('header') ?>
 
   <section class="page">
-    <?= $page->text()->kt(); ?>
+    <?= $page->text()->kt() ?>
     <hr>
     <div class="classics">
       <?php
@@ -12,18 +12,35 @@
       ?>
       <div class="classic<?php e($count++%2, ' left', ' right') ?> wrap">
         <div class="image">
-          <?php if($image = $item->image()->toFile()) { echo $image->imageset([ '345' ], [ 'alt' => $image->img_desc() ]); } ?>
+          <?php
+            $images = $item->thumbs()->yaml();
+            $first = a::first($images);
+            $thumb = $page->image($first);
+          ?>
+          <?= $thumb->crop(345, 230, 85)->html() ?>
+          <div class="thumbnails" style="wdith: 345px">
+            <?php foreach ($images as $image) : ?>
+            <?php if($image = $page->image($image)) : ?>
+                <?= $image->crop(112, 112, 85)->html(); ?>
+            <?php endif ?>
+            <?php endforeach ?>
+          </div>
+
+
+
+
+
         </div>
         <div class="text">
           <h2><?= $item->title() ?></h2>
-          <?= $item->text()->kt(); ?>
+          <?= $item->text()->kt() ?>
         </div>
       </div>
       <?php e($item !== $last, '<hr>') ?>
       <?php endforeach ?>
     </div>
     <hr>
-    <?= $page->outro()->kt(); ?>
+    <?= $page->outro()->kt() ?>
   </section>
 
 <?php snippet('footer') ?>
