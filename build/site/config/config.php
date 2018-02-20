c6a1af3ed5dc8273d2bbfd10cb645982eaeb0f23
M. Folkers <hello@twobrain.io>
2018-02-20 21:42:41 +0100

    Adding auto-resize on file upload

--- build/site/config/config.php
+++ build/site/config/config.php
@@ -38,3 +38,34 @@ of the system, please check out http://getkirby.com/docs/advanced/options
  */
 
 c::set('panel.stylesheet', 'assets/panel.css');
+
+
+c::set('govinda.upload-quality', 85);
+
+/*
+ * Resizing images on upload / replacement
+ */
+
+kirby()->hook('panel.file.upload', 'resizeImage');
+kirby()->hook('panel.file.replace', 'resizeImage');
+
+function resizeImage($file) {
+  // set a max. dimension
+  $maxDimension = 1440;
+  try {
+    // check file type and dimensions
+    if ($file->type() == 'image' and ($file->width() > $maxDimension)) {
+
+      // get the original file path
+      $originalPath = $file->dir() . '/' . $file->filename();
+      // create a thumb and get its path
+      $resizedImage = $file->resize($maxDimension, null, c::get('govinda.upload-quality'));
+      $resizedPath = $resizedImage->dir() . '/' . $resizedImage->filename();
+      // replace the original image with the resized one
+      copy($resizedPath, $originalPath);
+      unlink($resizedPath);
+      }
+  } catch (Exception $e) {
+      return response::error($e->getMessage());
+  }
+}
