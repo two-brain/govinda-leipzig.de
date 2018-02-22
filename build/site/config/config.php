01b4224ebe7106eec29dd5d7e8c9d9385db7ae0e
Martin Folkers <hello@twobrain.io>
2018-02-22 16:53:06 +0100

    (Re)Moving media-related stuff

--- build/site/config/config.php
+++ build/site/config/config.php
@@ -4,7 +4,7 @@
 
 include kirby()->roots()->config() . '/license.php';
 include kirby()->roots()->config() . '/languages.php';
-include kirby()->roots()->config() . '/thumbnails.php';
+include kirby()->roots()->config() . '/media.php';
 include kirby()->roots()->config() . '/plugins.php';
 
 /*
@@ -38,34 +38,3 @@ of the system, please check out http://getkirby.com/docs/advanced/options
  */
 
 c::set('panel.stylesheet', 'assets/panel.css');
-
-
-c::set('govinda.upload-quality', 85);
-
-/*
- * Resizing images on upload / replacement
- */
-
-kirby()->hook('panel.file.upload', 'resizeImage');
-kirby()->hook('panel.file.replace', 'resizeImage');
-
-function resizeImage($file) {
-  // set a max. dimension
-  $maxDimension = 1440;
-  try {
-    // check file type and dimensions
-    if ($file->type() == 'image' and ($file->width() > $maxDimension)) {
-
-      // get the original file path
-      $originalPath = $file->dir() . '/' . $file->filename();
-      // create a thumb and get its path
-      $resizedImage = $file->resize($maxDimension, null, c::get('govinda.upload-quality'));
-      $resizedPath = $resizedImage->dir() . '/' . $resizedImage->filename();
-      // replace the original image with the resized one
-      copy($resizedPath, $originalPath);
-      unlink($resizedPath);
-      }
-  } catch (Exception $e) {
-      return response::error($e->getMessage());
-  }
-}
