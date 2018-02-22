d1eee599265a4b3f220dfb550df19f1e9e290788
Martin Folkers <hello@twobrain.io>
2018-02-22 16:53:26 +0100

    Deleting thumbnails.php

--- build/site/config/thumbnails.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-c::set('thumbs.driver', 'im');
-c::set('thumbs.quality', 85);
-
-// ImageSet options - see https://github.com/fabianmichael/kirby-imageset
-c::set('imageset.placeholder', 'blurred');
