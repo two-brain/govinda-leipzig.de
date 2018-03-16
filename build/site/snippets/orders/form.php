d5b8a23a3ba7e37d231895eb2b8483516b60b7fa
M. Folkers <hello@twobrain.io>
2018-03-16 10:37:38 +0100

    Changing everything formal to informal Also removing form placeholder

--- build/site/snippets/orders/form.php
+++ build/site/snippets/orders/form.php
@@ -5,29 +5,29 @@
     <div class="wrap clearfix">
       <div class="form__item">
         <label for="full_name">Name:<span class="required"> (erforderlich)</span></label>
-        <input type="text" id="full_name" name="full_name" placeholder="Wer fragt an?" value="<?= isset($data['full_name']) ? $data['full_name'] : '' ?>" required />
+        <input type="text" id="full_name" name="full_name" value="<?= isset($data['full_name']) ? $data['full_name'] : '' ?>" required />
         <div class="alert"><?php if(isset($response['errors']['full_name'])) { echo $response['errors']['full_name']; } ?></div>
       </div>
 
       <div class="form__item">
         <label for="email">Email:<span class="required"> (erforderlich)</span></label>
-        <input type="email" name="email" id="email" placeholder="mail@beispiel.de" value="<?= isset($data['email']) ? $data['email'] : '' ?>" required />
+        <input type="email" name="email" id="email" value="<?= isset($data['email']) ? $data['email'] : '' ?>" required />
         <div class="alert"><?php if(isset($response['errors']['email'])) { echo $response['errors']['email']; } ?></div>
       </div>
 
       <div class="form__item">
         <label for="telefon">Telefon:</label>
-        <input type="text" id="telefon" name="telefon" placeholder="Mit Telefonnummer geht's schneller!" value="<?= isset($data['telefon']) ? $data['telefon'] : '' ?>"/>
+        <input type="text" id="telefon" name="telefon" value="<?= isset($data['telefon']) ? $data['telefon'] : '' ?>"/>
       </div>
 
       <div class="form__item">
         <label for="datum">Datum der Veranstaltung:</label>
-        <input type="text" id="datum" name="datum" placeholder="Wann findet Ihre Veranstaltung statt?" value="<?= isset($data['datum']) ? $data['datum'] : '' ?>"/>
+        <input type="text" id="datum" name="datum" value="<?= isset($data['datum']) ? $data['datum'] : '' ?>"/>
       </div>
 
       <div class="form__item">
         <label for="art">Art der Veranstaltung:</label>
-        <input type="text" id="art" name="art" placeholder="zB Hochzeit, Jugendweihe, .." value="<?= isset($data['art']) ? $data['art'] : '' ?>"/>
+        <input type="text" id="art" name="art" value="<?= isset($data['art']) ? $data['art'] : '' ?>"/>
       </div>
 
       <div class="form__item">
